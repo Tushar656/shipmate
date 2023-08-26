4d2e71e5c04583bb8ba1838c67641609a1ccf9af
Tushar656 <ui20cs69@iiitsurat.ac.in>
2023-08-26 21:52:15 +0530

    recheck

--- app/layout.js
+++ app/layout.js
@@ -14,8 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <GlobalReduxProvider>
-
-        {children}
+          {children}
         </GlobalReduxProvider>
       </body>
     </html>
